c1082f4f8c942d249503e39434bbc908b32f22f7
Mahmoud Gamal <mgamal@sgs-uae.com>
2024-01-18 13:06:01 +0200

    Eslint Config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,40 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
-};
+    "env": {
+        "browser": true,
+        "amd": true,
+        "node": true,
+        "es2021": true,
+        "jest/globals": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:jest/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "jest",
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+    }
+}
